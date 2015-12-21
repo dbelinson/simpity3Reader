9f5fb0393861cbcbf31a223e27ecaf616d711e42
Dmitrij <belinson@mail.ru>
2015-12-21 15:24:04 +0300

    Menu Anderung 1

---
+++ app/src/main/java/it/angrydroids/epub3reader/About.java
@@ -0,0 +1,13 @@
+package it.angrydroids.epub3reader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_view);
+    }
+}
